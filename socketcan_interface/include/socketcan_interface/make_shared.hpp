ca87d9d05520e868f9854192018e31cf4310781c
Luis Camero <lcamero@clearpathrobotics.com>
2024-06-11 12:08:50 -0400

    Added socketcan_interface

---
+++ socketcan_interface/include/socketcan_interface/make_shared.hpp
@@ -0,0 +1,22 @@
+// Copyright (c) 2019 Mathias Lüdtke
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef SOCKETCAN_INTERFACE__MAKE_SHARED_HPP_
+#define SOCKETCAN_INTERFACE__MAKE_SHARED_HPP_
+
+#include <memory>
+#define ROSCANOPEN_MAKE_SHARED std::make_shared
+
+#endif  // SOCKETCAN_INTERFACE__MAKE_SHARED_HPP_
